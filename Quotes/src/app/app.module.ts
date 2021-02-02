e7f0c833abf09312c1ff57cc195e291e2227cf92
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-02 09:37:57 +0300

    initial commit

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
