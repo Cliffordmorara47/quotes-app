95e5c564997e3746ae7fdc04cfc2df51514accc6
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-02 13:16:39 +0300

    added a form component

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -13,10 +13,17 @@ export class QuoteComponent implements OnInit {
     new Quote(0, "Tough Moments", "Don't worry about the things or people that you have lost in your life, things and people who are meant to be with you are with you", "Clifford Morara", 0, 0),
     new Quote(1, "Tough Moments", "When you are broken, its very hard to feel special", "Martin Garrix", 0, 0),
     new Quote(2, "Encouragement", "I would rather die on my feet than live on my knees", "Nelson Mandela", 0, 0),
-    new Quote(3, "Heed", "The world is a place that has a great risks and fairly great opportunities", "George Bush", 0, 0)
-
-    
+    new Quote(3, "Heed", "The world is a place that has a great risks and fairly great opportunities", "George Bush", 0, 0) 
   ]
+  deleteQuote(isComplete: any, index:any) {
+    if (isComplete) {
+      let toDelete = confirm(`Confirm deletion ${this.Quotes[index].name}?`)
+
+      if (toDelete) {
+        this.Quotes.splice(index, 1)
+      }
+    }
+  }
 
 
   constructor() { }
